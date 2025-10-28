e2268e0a0517dba7cd166c0d22129f91aeedd647
Eduardo roman <student.160097@worcesterschools.net>
2025-10-28 08:58:21 -0400

    update

--- main.py
+++ main.py
@@ -1,19 +1,40 @@
 from faker import Faker # installing faker and importing it into the code
 
 fake = Faker()
-y = input("Would you like me to generate you an person yes/no? ") # my input statement
-
-if y == "yes": 
-    print(fake.name())
-    print(fake.job())
-    print(fake.date())
-    print(fake.country())
-    print(fake.state())
-    print(fake.city())
-    print(fake.zipcode())
-else:
-    print("What would like me to generate then here are some examples I can do random dates ⏱️, time's ⌛ or even phone numbers 📞")
-input("date's/dates/date").lower
-input("time's/times/time").lower
-input("phone number's/phone numbers/phone number").lower
+print("Hello and welcome to my random name generator")
+print("Are you a female or male")
 input("")
+print("What is your name")
+input("")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# y = input("Would you like me to generate you an person yes/no? ") # my input statement
+
+# if y == "yes": 
+#     print(fake.name())
+#     print(fake.date())
+#      print(fake.job())
+#     print(fake.city())
+#     print(fake.state())
+#     print(fake.country())
+#     print(fake.zipcode())
+# else:
+#     print("What would like me to generate then here are some examples I can do random dates ⏱️, time's ⌛ or even phone numbers 📞")
+# input("date's/dates/date").lower
+# input("time's/times/time").lower
+# input("phone number's/phone numbers/phone number").lower
+# input("")
