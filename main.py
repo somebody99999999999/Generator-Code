076e9c3836e90eca6cf62a646ea32cdedf742e44
Eduardo roman <student.160097@worcesterschools.net>
2025-10-31 10:55:04 -0400

    update

--- main.py
+++ main.py
@@ -10,38 +10,7 @@ else:
     print("Good to know that your a female/woman")
 print("What is your name")
 name = input("")
-if name == "john":
+if name == "john/John":
     print("Your new name is now Johnson")
 else:
-    print("")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# y = input("Would you like me to generate you an person yes/no? ") # my input statement
-
-# if y == "yes": 
-#     print(fake.name())
-#     print(fake.date())
-#      print(fake.job())
-#     print(fake.city())
-#     print(fake.state())
-#     print(fake.country())
-#     print(fake.zipcode())
-# else:
-#     print("What would like me to generate then here are some examples I can do random dates ⏱️, time's ⌛ or even phone numbers 📞")
-# input("date's/dates/date").lower
-# input("time's/times/time").lower
-# input("phone number's/phone numbers/phone number").lower
-# input("")
+    print("Your new name is Jackson")
